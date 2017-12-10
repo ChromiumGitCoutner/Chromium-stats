3521ad66af4d6a78f3e96f82c712805d4398d98b
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-12-10 18:23:24 +0900

    Update runChromiumAnalyzer.sh

--- runChromiumGitAnalyzer.sh
+++ runChromiumGitAnalyzer.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Define pathes for this tool and Chromium source.
-GIT_COUNTER_PATH=$HOME/github/Chromium-stats
+GIT_COUNTER_PATH=$HOME/github/igaila/igalia-chromium-contribution-stats
 CHROMIUM_PATH=$HOME/chromium-stats/chromium
-GIT_INSPECTOR_PATH=$HOME/github/gitinspector
+GIT_INSPECTOR_PATH=$HOME/github/igaila/gitinspector-for-igalia-chromium-stats
 INDEX_ORG_PATH=$HOME/chromium-stats/log
- START_DATE="2017-01-01"
+START_DATE="2017-01-01"
 export PATH=$GIT_INSPECTOR_PATH:$PATH
 
 while :
