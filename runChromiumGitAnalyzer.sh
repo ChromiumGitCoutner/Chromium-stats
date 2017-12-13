c8fbb3fcf242669212bb82e892a9e1e79236df57
Gyuyoung Kim <gyuyoung.kim@lge.com>
2017-12-13 12:26:12 +0900

    Change start date

--- runChromiumGitAnalyzer.sh
+++ runChromiumGitAnalyzer.sh
@@ -5,7 +5,7 @@ GIT_COUNTER_PATH=$HOME/github/igaila/igalia-chromium-contribution-stats
 CHROMIUM_PATH=$HOME/chromium-stats/chromium
 GIT_INSPECTOR_PATH=$HOME/github/igaila/gitinspector-for-igalia-chromium-stats
 INDEX_ORG_PATH=$HOME/chromium-stats/log
-START_DATE="2017-01-01"
+START_DATE="2012-01-01"
 export PATH=$GIT_INSPECTOR_PATH:$PATH
 
 while :
